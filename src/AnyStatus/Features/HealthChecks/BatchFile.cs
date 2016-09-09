6a9df9a092cc872352228ff9c55e6d7736ab3b74
AlonAm <alon.amsalem@gmail.com>
2016-09-09 23:35:25 +0300

    batch file status

--- src/AnyStatus/Features/HealthChecks/BatchFile.cs
+++ src/AnyStatus/Features/HealthChecks/BatchFile.cs
@@ -1,23 +1,56 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Media;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
 namespace AnyStatus.Models
 {
-    [Browsable(false)]
     [DisplayName("Batch File")]
-    [Description("Execute a batch file")]
+    [Description("Execute a batch file.")]
     public class BatchFile : Item, IScheduledItem
     {
-        [DisplayName("File Path")]
-        [Description("The batch file path")]
+        public BatchFile()
+        {
+            Timeout = 1;
+        }
+
+        [PropertyOrder(10)]
         [Category("Batch File")]
-        public string FilePath { get; set; }
+        [DisplayName("File Name")]
+        [Description("The batch file full path")]
+        [Editor(typeof(FileEditor), typeof(FileEditor))]
+        public string FileName { get; set; }
+
+        [PropertyOrder(20)]
+        [Category("Batch File")]
+        [Description("The batch file arguments")]
+        public string Arguments { get; set; }
+
+        [PropertyOrder(30)]
+        [Category("Batch File")]
+        [DisplayName("Timeout (minutes)")]
+        [Description("The script execution timeout in minutes")]
+        public int Timeout { get; set; }
     }
 
     public class BatchFileHandler : IHandler<BatchFile>
     {
         public void Handle(BatchFile item)
         {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = item.FileName,
+                CreateNoWindow = true,
+                ErrorDialog = false,
+                LoadUserProfile = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+            };
 
+            var process = Process.Start(startInfo);
+
+            process.WaitForExit(item.Timeout * 60 * 1000);
+
+            item.Brush = process.ExitCode == 0 ? Brushes.Green : Brushes.Red;
         }
     }
 }
---
+++ src/AnyStatus/Features/HealthChecks/Editors/FileEditor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
+namespace AnyStatus
+{
+    public class FileEditor : ITypeEditor
+    {
+        public FrameworkElement ResolveEditor(PropertyItem propertyItem)
+        {
+            Grid grid = new Grid();
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+
+            TextBox textBox = new TextBox();
+            textBox.HorizontalAlignment = HorizontalAlignment.Stretch;
+
+            Binding binding = new Binding("Value"); //bind to the Value property of the PropertyItem
+            binding.Source = propertyItem;
+            binding.Mode = BindingMode.TwoWay;
+            BindingOperations.SetBinding(textBox, TextBox.TextProperty, binding);
+
+            Button button = new Button();
+            button.Content = "...";
+            button.Tag = propertyItem;
+            button.Click += button_Click;
+            Grid.SetColumn(button, 1);
+
+            grid.Children.Add(textBox);
+            grid.Children.Add(button);
+
+            return grid;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            PropertyItem item = ((Button)sender).Tag as PropertyItem;
+
+            if (null == item) return;
+
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            item.Value = openFileDialog.FileName;
+        }
+    }
+
+}
