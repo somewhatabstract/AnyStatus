0e59665a73436cbb67ff6170fc74c779ae1dbeed
AlonAm <alon.amsalem@gmail.com>
2016-09-10 13:29:29 +0300

    powershell script execution policy

--- src/AnyStatus/Features/HealthChecks/PowerShellScript.cs
+++ src/AnyStatus/Features/HealthChecks/PowerShellScript.cs
@@ -14,47 +14,70 @@ namespace AnyStatus.Models
         public PowerShellScript()
         {
             Timeout = 1;
+            BypassExecutionPolicy = true;
         }
 
         [Required]
         [PropertyOrder(10)]
-        [Category("PowerShell")]
+        [Category("PowerShell Script")]
         [DisplayName("File Name")]
         [Description("The script file path")]
         [Editor(typeof(FileEditor), typeof(FileEditor))]
         public string FileName { get; set; }
 
         [PropertyOrder(20)]
-        [Category("PowerShell")]
+        [Category("PowerShell Script")]
         [Description("The script arguments")]
         public string Arguments { get; set; }
 
         [PropertyOrder(30)]
-        [Category("PowerShell")]
+        [Category("PowerShell Script")]
         [Description("The script execution timeout in minutes")]
         public int Timeout { get; set; }
+
+        [ReadOnly(true)]
+        [PropertyOrder(40)]
+        [Category("PowerShell Script")]
+        [DisplayName("Bypass Execution Policy")]
+        [Description("Bypass PowerShell execution policy")]
+        public bool BypassExecutionPolicy { get; set; }
     }
 
     public class PowerShellScriptHandler : IHandler<PowerShellScript>
     {
+        private ILogger _logger;
+
+        public PowerShellScriptHandler(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public void Handle(PowerShellScript item)
         {
             if (!File.Exists(item.FileName))
                 throw new FileNotFoundException(item.FileName);
 
+            var executionPolicy = item.BypassExecutionPolicy ? "ByPass" : "Restricted";
+
             var startInfo = new ProcessStartInfo
             {
                 FileName = "powershell.exe",
-                Arguments = $"&'{item.FileName}' {item.Arguments}",
+                Arguments = $"-ExecutionPolicy {executionPolicy} -File \"{item.FileName}\" {item.Arguments}",
                 CreateNoWindow = true,
                 ErrorDialog = false,
                 LoadUserProfile = false,
                 WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
             };
 
             var process = Process.Start(startInfo);
 
             process.WaitForExit(item.Timeout * 60 * 1000);
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            _logger.Info(output);
 
             item.Brush = process.ExitCode == 0 ? Brushes.Green : Brushes.Red;
         }
