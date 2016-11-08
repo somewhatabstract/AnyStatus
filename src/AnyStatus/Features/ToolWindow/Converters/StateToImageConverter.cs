97117ac96cfe2e56090f35c996946f39d01f5beb
AlonAm <alon.amsalem@gmail.com>
2016-11-09 00:06:18 +0200

    status icons #17

---
+++ src/AnyStatus/Features/ToolWindow/Converters/StateToImageConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace AnyStatus
+{
+    public class StateToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ItemState))
+                return null;
+
+            var state = (ItemState)value;
+
+            var image = GetImageName(state);
+
+            return new BitmapImage(new Uri("pack://application:,,,/AnyStatus;component/Resources/Icons/" + image));
+        }
+
+        private static string GetImageName(ItemState state)
+        {
+            switch (state)
+            {
+                default:
+                case ItemState.None:
+                    return "Blank.png";
+                case ItemState.Unknown:
+                    return "StatusHelp_gray_16x.png";
+                case ItemState.Disabled:
+                    return "StatusPause_grey_16x.png";
+                case ItemState.Canceled:
+                    return "StatusStop_grey_16x.png";
+                case ItemState.Ok:
+                    return "StatusOK_grey_16x.png";
+                case ItemState.Open:
+                    return "StatusInformation_grey_16x.png";
+                case ItemState.Closed:
+                    return "StatusOK_grey_16x.png";
+                case ItemState.PartiallySucceeded:
+                    return "StatusInvalid_grey_16x";
+                case ItemState.Failed:
+                    return "StatusCriticalError_grey_16x.png";
+                case ItemState.Invalid:
+                    return "StatusWarning_grey_16x.png";
+                case ItemState.Error:
+                    return "StatusWarning_grey_16x.png";
+                case ItemState.Running:
+                    return "StatusRun_grey_16x.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AnyStatus/Features/ToolWindow/Converters/StateToMonikerConverter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AnyStatus
+{
+    public class StateToMonikerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var state = (ItemState)value;
+            
+            switch (state)
+            {
+                //Monikers List: http://glyphlist.azurewebsites.net/knownmonikers/
+
+                default:
+                case ItemState.None:
+                    return KnownMonikers.Blank;
+
+                case ItemState.Unknown:
+                    return KnownMonikers.StatusHelp;
+
+                case ItemState.Disabled:
+                    return KnownMonikers.StatusPaused;
+
+                case ItemState.Canceled:
+                    return KnownMonikers.StatusStoppedOutline;
+
+                case ItemState.Ok:
+                    return KnownMonikers.StatusOKNoColor;
+
+                case ItemState.Open:
+                    return KnownMonikers.StatusInformationOutline;
+
+                case ItemState.Closed:
+                    return KnownMonikers.StatusInvalidOutline;
+
+                case ItemState.PartiallySucceeded:
+                    return KnownMonikers.StatusWarningNoColor;
+
+                case ItemState.Failed:
+                    return KnownMonikers.StatusErrorNoColor;
+
+                case ItemState.Invalid:
+                    return KnownMonikers.StatusInformationNoColor;
+
+                case ItemState.Error:
+                    return KnownMonikers.StatusWarningNoColor;
+
+                case ItemState.Running:
+                    return KnownMonikers.StatusRunningNoColor;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
