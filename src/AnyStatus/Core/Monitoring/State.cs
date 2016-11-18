03365e38ef1cc4c804fdaeb8a588c7d706c11793
AlonAm <alon.amsalem@gmail.com>
2016-11-18 12:38:58 +0200

    vector icons

--- src/AnyStatus/Core/Monitoring/State.cs
+++ src/AnyStatus/Core/Monitoring/State.cs
@@ -8,18 +8,18 @@ namespace AnyStatus
     {
         private StateMetadata _stateMetadata;
 
-        public static readonly State None = new State(0, 0, "None", "Silver", "Blank.png");
-        public static readonly State Unknown = new State(1, 1, "Unknown", "Silver", "StatusHelp_gray_16x.png");
-        public static readonly State Disabled = new State(2, 2, "Disabled", "Silver", "StatusPause_grey_16x.png");
-        public static readonly State Canceled = new State(3, 3, "Canceled", "Gray", "StatusStop_grey_16x.png");
-        public static readonly State Ok = new State(4, 4, "Ok", "Green", "StatusOK_grey_16x.png");
-        public static readonly State Open = new State(5, 5, "Open", "Green", "StatusInformation_grey_16x.png");
-        public static readonly State Closed = new State(6, 6, "Closed", "Red", "StatusOK_grey_16x.png");
-        public static readonly State PartiallySucceeded = new State(7, 7, "Partially Succeeded", "Orange", "StatusInvalid_grey_16x");
-        public static readonly State Failed = new State(8, 8, "Failed", "Red", "StatusCriticalError_grey_16x.png");
-        public static readonly State Invalid = new State(9, 9, "Invalid", "DarkRed", "StatusWarning_grey_16x.png");
-        public static readonly State Error = new State(10, 10, "Error", "DarkRed", "StatusWarning_grey_16x.png");
-        public static readonly State Running = new State(11, 11, "Running", "DodgerBlue", "StatusRun_grey_16x.png");
+        public static readonly State None = new State(0, 0, "None", "Silver", string.Empty);
+        public static readonly State Unknown = new State(1, 1, "Unknown", "Silver", "HelpIcon");
+        public static readonly State Disabled = new State(2, 2, "Disabled", "Silver", "PauseIcon");
+        public static readonly State Canceled = new State(3, 3, "Canceled", "Gray", "StopIcon");
+        public static readonly State Ok = new State(4, 4, "Ok", "Green", "OkIcon");
+        public static readonly State Open = new State(5, 5, "Open", "Green", "InfoIcon");
+        public static readonly State Closed = new State(6, 6, "Closed", "Red", "OkIcon");
+        public static readonly State PartiallySucceeded = new State(7, 7, "Partially Succeeded", "Orange", "PartiallySucceededIcon");
+        public static readonly State Failed = new State(8, 8, "Failed", "Red", "FailedIcon");
+        public static readonly State Invalid = new State(9, 9, "Invalid", "DarkRed", "WarningIcon");
+        public static readonly State Error = new State(10, 10, "Error", "DarkRed", "WarningIcon");
+        public static readonly State Running = new State(11, 11, "Running", "DodgerBlue", "RunningIcon");
 
         private State(int value, int priority, string displayName, string color, string icon) :
             base(value)
@@ -44,7 +44,7 @@ namespace AnyStatus
             {
                 if (states.ContainsKey(metadata.Value))
                 {
-                    states[metadata.Value].Metadata = metadata;
+                    states[metadata.Value].Metadata.Color = metadata.Color;
                 }
             }
         }
---
+++ src/AnyStatus/Infrastructure/Converters/IconConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Xaml;
+
+namespace AnyStatus
+{
+    public class IconConverter : MarkupExtension, IValueConverter
+    {
+        private Control _target;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value as string))
+                return null;
+
+            return _target?.TryFindResource((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var rootObjectProvider = serviceProvider.GetService(typeof(IRootObjectProvider)) as IRootObjectProvider;
+
+            if (rootObjectProvider == null)
+                return this;
+
+            _target = rootObjectProvider.RootObject as Control;
+
+            return this;
+        }
+    }
+}
