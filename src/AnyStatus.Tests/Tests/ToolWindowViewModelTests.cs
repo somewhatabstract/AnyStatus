152bfb1ec6abfb9291c9224dd59b440123ff547c
AlonAm <alon.amsalem@gmail.com>
2016-11-21 20:51:55 +0200

    tool window view model tests

---
+++ src/AnyStatus.Tests/Tests/ToolWindowViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Linq;
+using System.Windows.Input;
+
+namespace AnyStatus.Tests.Tests
+{
+    [TestClass]
+    public class ToolWindowViewModelTests
+    {
+        [TestMethod]
+        public void Initialize_Should_Set_All_Properties()
+        {
+            var settings = new UserSettings();
+            var logger = Substitute.For<ILogger>();
+            var viewLocator = Substitute.For<IViewLocator>();
+            var jobScheduler = Substitute.For<IJobScheduler>();
+            var settingsStore = Substitute.For<ISettingsStore>();
+
+            settingsStore.Settings.Returns(settings);
+
+            var viewModel = new ToolWindowViewModel(jobScheduler, settingsStore, viewLocator, logger);
+
+            var anyPropertyIsNull = viewModel.GetType().GetProperties()
+                                             .Select(pi => pi.GetValue(viewModel))
+                                             .Any(value => value == null);
+
+            Assert.IsFalse(anyPropertyIsNull);
+        }
+    }
+}
