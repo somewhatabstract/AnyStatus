b1ff8497de2a1a8644f7c89699940fd754dde9f4
AlonAm <alon.amsalem@gmail.com>
2016-11-21 12:32:37 +0200

    monitoring handlers tests

---
+++ src/AnyStatus.Integration.Tests/MonitoringHandlersTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnyStatus.Integration.Tests
+{
+    [TestClass]
+    public class MonitoringHandlersTests
+    {
+        [TestMethod]
+        public void HttpHandler()
+        {
+            var request = new HttpStatus { Url = "http://www.microsoft.com" };
+
+            var handler = new HttpStatusHandler();
+
+            handler.Handle(request);
+
+            Assert.AreSame(State.Ok, request.State);
+        }
+
+        [TestMethod]
+        public void PingHandler()
+        {
+            var request = new Ping { Host = "localhost" };
+
+            var handler = new PingHandler();
+
+            handler.Handle(request);
+
+            Assert.AreSame(State.Ok, request.State);
+        }
+
+        [TestMethod]
+        public void TcpHandler()
+        {
+            var request = new TcpPort
+            {
+                Host = "www.microsoft.com",
+                Port = 80
+            };
+
+            var handler = new TcpPortHandler();
+
+            handler.Handle(request);
+
+            Assert.AreSame(State.Ok, request.State);
+        }
+
+        [TestMethod]
+        public void WindowsServiceHandler()
+        {
+            var request = new WindowsService
+            {
+                ServiceName = "Dhcp"
+            };
+
+            var handler = new WindowsServiceHandler();
+
+            handler.Handle(request);
+
+            Assert.AreSame(State.Ok, request.State);
+        }
+    }
+}
