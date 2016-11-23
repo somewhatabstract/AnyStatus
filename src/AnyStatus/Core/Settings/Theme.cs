d6918c0384842e4b789318f60cd4b8d77f025ffc
AlonAm <alon.amsalem@gmail.com>
2016-11-23 14:01:02 +0200

    theme icons and colors

--- src/AnyStatus/Core/Settings/Theme.cs
+++ src/AnyStatus/Core/Settings/Theme.cs
@@ -14,12 +14,12 @@ namespace AnyStatus
                         new StateMetadata(State.Unknown, 1, "Unknown", "Silver", "HelpIcon"),
                         new StateMetadata(State.Disabled, 2, "Disabled", "#FF696969", "PauseIcon"),
                         new StateMetadata(State.Canceled, 3, "Canceled", "#FF808080", "StopIcon"),
-                        new StateMetadata(State.Ok, 4, "Ok", "#FF32CD32", "OkIcon"),
-                        new StateMetadata(State.Open, 5, "Open", "#FF32CD32", "OpenIcon"),
+                        new StateMetadata(State.Ok, 4, "Ok", "Green", "OkIcon"),
+                        new StateMetadata(State.Open, 5, "Open", "Green", "OpenIcon"),
                         new StateMetadata(State.Closed, 6, "Closed", "Red", "ClosedIcon"),
-                        new StateMetadata(State.PartiallySucceeded, 7, "Partially Succeeded", "Yellow", "PartiallySucceededIcon"),
+                        new StateMetadata(State.PartiallySucceeded, 7, "Partially Succeeded", "Orange", "PartiallySucceededIcon"),
                         new StateMetadata(State.Failed, 8, "Failed", "Red", "FailedIcon"),
-                        new StateMetadata(State.Invalid, 9, "Invalid", "Orange", "WarningIcon"),
+                        new StateMetadata(State.Invalid, 9, "Invalid", "Red", "WarningIcon"),
                         new StateMetadata(State.Error, 10, "Error", "Red", "ErrorIcon"),
                         new StateMetadata(State.Running, 11, "Running", "DodgerBlue", "RunIcon")
                     }
