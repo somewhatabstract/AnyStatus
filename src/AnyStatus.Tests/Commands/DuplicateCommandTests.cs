cdb2ba5d08c05387c1a63797da1d6c95ff021c97
AlonAm <alon.amsalem@gmail.com>
2016-11-26 18:33:09 +0200

    duplicate command tests

---
+++ src/AnyStatus.Tests/Commands/DuplicateCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace AnyStatus.Tests.Commands
+{
+    [TestClass]
+    public class DuplicateCommandTests
+    {
+        [TestMethod]
+        public void Should_Duplication_Schedule_And_Save()
+        {
+            var jobScheduler = Substitute.For<IJobScheduler>();
+            var settingsStore = Substitute.For<ISettingsStore>();
+
+            var parent = new Item();
+            var item = new Item();
+
+            parent.Add(item);
+
+            var command = new DuplicateCommand(item);
+            var handler = new DuplicateCommandHandler(jobScheduler, settingsStore);
+
+            handler.Handle(command);
+
+            Assert.IsNotNull(parent.Items.ElementAtOrDefault(1));
+
+            jobScheduler.Received().Schedule(parent.Items[1], false);
+
+            settingsStore.Received().TrySave();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Should_Throw_When_CommandIsNull()
+        {
+            var jobScheduler = NSubstitute.Substitute.For<IJobScheduler>();
+            var settingsStore = NSubstitute.Substitute.For<ISettingsStore>();
+
+            var handler = new DuplicateCommandHandler(jobScheduler, settingsStore);
+
+            handler.Handle(null);
+        }
+    }
+}
